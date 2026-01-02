ee247791bb57a95c644d2e82ee58162fb27b708c
hurxxxx <hurxxxx@gmail.com>
2026-01-02 16:35:57 +0900

    .

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const imageHost = process.env.IMAGE_REMOTE_HOST || "gc.lumejs.com";
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "gc.lumejs.com",
+        hostname: imageHost,
         pathname: "/uploads/**",
       },
     ],
